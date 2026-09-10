d4587c9d4c21b4a29245a171cd8de0d3d9873e89
Hulgo <hulgo.ferreira@outlook.com>
2026-09-10 20:04:51 -0300

    Add .github

--- tests/pages/cart_page.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By 
-from .base_page import BasePage 
-  
-class CartPage(BasePage): 
-  
-    CHECKOUT = (By.ID, "checkout") 
-  
-    def start_checkout(self): 
-        self.click(*self.CHECKOUT) 
--- tests/specs/login/test_login_transaction.py
+++
@@ -1,9 +0,0 @@
-from guara.transaction import AbstractTransaction
-from tests.pages.login_page import LoginPage
-
-class LoginWith(AbstractTransaction):
-    def do(self, url, user, password):
-        self._driver.get(url)
-        page = LoginPage(self._driver)
-        page.login(user, password)
-        return self._driver.current_url
